a81c0b3a08a7b574aa243ac1bd1bc97f746bf1a1
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-12 11:27:36 +0530

    The Maximum and Minimum Element

---
+++ MaxMin.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    internal class MaxMin
+    {
+        public static void MaxMinMain()
+        {
+            
+            Console.WriteLine("Maximum Element " + FindMax(10.5f, 80.2f, 30.6f));
+            Console.WriteLine("Maximum Element " + FindMax(15.222d, 65.222d, 569.2050d));
+            Console.WriteLine("Maximum Element " + FindMax(10, 20, 30));
+
+            Console.WriteLine("Minimum Element " + FindMin(10.5f, 80.2f, 30.6f));
+            Console.WriteLine("Minimum Element " + FindMin(15.222d, 65.222d, 569.2050d));
+            Console.WriteLine("Minimum Element " + FindMin(10, 20, 30));
+        }
+        public static T FindMax<T>(T input1, T input2, T input3) where T : IComparable<T>
+        {
+            T max = input1;
+            if (input2.CompareTo(max) > 0) max = input2;
+            if (input3.CompareTo(max) > 0) max = input3;
+            return max;
+        }
+
+        public static T FindMin<T>(T input1, T input2, T input3) where T : IComparable<T>
+        {
+            T min = input1;
+            if (input2.CompareTo(min) < 0) min = input2;
+            if (input3.CompareTo(min) < 0) min = input3;
+            return min;
+        }
+    }
+}

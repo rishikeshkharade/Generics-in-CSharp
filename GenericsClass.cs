fa982425b99d07c7fe01f64575a019662a877c64
RISHIKESH KHARADE <152712515+rishikeshkharade@users.noreply.github.com>
2025-02-12 11:26:41 +0530

    Generics Basics

---
+++ GenericsClass.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class GenericsClass
+    {
+        public static void GenericsClassMain()
+        {
+            int[] intArr = { 1, 2, 3, 4, 5, };
+            char[] charArr = { 'a', 'b', 'c', 'd' };
+            double[] doubleArr = { 1.2, 3.2, 7.8, 9.0 };
+            string[] strArr = { "Rishikesh", "Ashok", "Kharade" };
+            ToPrint(intArr);
+            ToPrint(charArr);
+            ToPrint(doubleArr);
+            ToPrint(strArr);
+        }
+        public static void ToPrint<T>(T[] array)
+        {
+            foreach (var item in array)
+            {
+                Console.WriteLine(item+" Generic");
+            }
+        }
+    public static void ToPrint(int[] intarray)
+            {
+                foreach (int elements in intarray)
+                {
+                    Console.WriteLine(elements +" Not Generic");
+                }
+            }
+        }
+    }
---
+++ GenericsReference.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics
+{
+    public class GenericsReference
+    {
+        public static void ClsMain()
+        {
+            bool IsEqual = ClsCalculator.AreEqual<string>("Rishi", "Rishi");
+            if (IsEqual)
+            {
+                Console.WriteLine("Value is Equal");
+            }
+            else
+            {
+                Console.WriteLine("Values are not Equal");
+            }
+
+            Console.ReadKey();
+        }
+    }
+
+    public class ClsCalculator
+    {
+        public static bool AreEqual<T>(T value1, T value2)
+        { return value1.Equals(value2); }
+    }
+}

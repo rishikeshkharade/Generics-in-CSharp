0347a3449442400d0d1d08f0bb2989acf7b937cd
RISHIKESH KHARADE <rishikeshkharade19@gmail.com>
2025-02-12 10:20:31 +0530

    Welcome to Generics Program

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+namespace Generics;
+    public class AllMain
+    {
+        static void Main(string[] args)
+        {
+        // GenericsClass.GenericsClassMain();
+        // GenericsReference.ClsMain();
+        // SwapNumbers.Swap();
+        // Compare.DifferentValues();
+        // MaxMin.MaxMinMain();
+        // SumOfTwoNumbers.SumOfTwoNumbersMain();
+        Calculator.CalculatorMain();
+        }
+    }
